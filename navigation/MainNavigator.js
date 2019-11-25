3b0f05ec70acd170f0739caf52b87fd77991c83d
Lee Jae Hun <wogns5206@naver.com>
2019-11-25 14:14:46 +0900

    sideBar_icon

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -18,23 +18,43 @@ const TestDraw = createDrawerNavigator(
         },
         칭호: {
             screen: Honor,
+            navigationOptions: {
+                title: "칭호",
+                drawerIcon: ({ tintColor }) => <Image source={require("../assets/images/medal.png")} style={{ width: 45, height: 45 }} color={tintColor} />
+            }
         },
         알림함: {
             screen: MailBox,
+            navigationOptions: {
+                title: "알림함",
+                drawerIcon: ({ tintColor }) => <Image source={require("../assets/images/alarm.png")} style={{ width: 45, height: 45 }} color={tintColor} />
+            }
         },
         인벤토리: {
             screen: Inventory,
+            navigationOptions: {
+                title: "인벤토리",
+                drawerIcon: ({ tintColor }) => <Image source={require("../assets/images/inventory.png")} style={{ width: 45, height: 45 }} color={tintColor} />
+            }
         },
         미션기록: {
             screen: MissionRecord,
+            navigationOptions: {
+                title: "미션기록",
+                drawerIcon: ({ tintColor }) => <Image source={require("../assets/images/missionRecord.png")} style={{ width: 45, height: 45 }} color={tintColor} />
+            }
         },
         지갑: {
             screen: Wallet,
+            navigationOptions: {
+                title: "지갑",
+                drawerIcon: ({ tintColor }) => <Image source={require("../assets/images/wallet.png")} style={{ width: 45, height: 45 }} color={tintColor} />
+            }
         },
     },
     {
         contentComponent: props => <SideBar {...props} />,
-        drawerWidth: Dimensions.get("window").width * 0.85,
+        drawerWidth: Dimensions.get("window").width * 0.75,
     }
 );
 
