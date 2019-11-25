6e0e3587389231c44bf29b959f3ccc0e5c107dc3
sungwoo1548 <swo1548@gmail.com>
2019-11-25 15:21:39 +0900

    drag button done

--- screens/main/MissionRecord.js
+++ screens/main/MissionRecord.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { Image, View, Text, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
+import img_drawer from '../../assets/images/drawer.png';
 
 const StatusBarH = StatusBar.currentHeight;
 
-export default class missionRecord extends React.Component {
+export default class MissionRecord extends React.Component {
     render() {
+        const { navigation } = this.props;
         return (
             <View style={styles.container}>
+                {/** 왼쪽 상단 버튼 */}
+                <View style={overStyle.topLeftSection}>
+                    <TouchableOpacity
+                        style={overStyle.topLeftButton}
+                        onPress={navigation.openDrawer}>
+                        <Image style={overStyle.topLeftImg} source={img_drawer} />
+                    </TouchableOpacity>
+                </View>
+
+
                 <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                    <Text style={styles.text}>{this.props.name} 미션기록</Text>
+                    <Text style={styles.text}>{this.props.name} 미션 기록</Text>
                 </View>
 
             </View>
@@ -20,12 +31,33 @@ export default class missionRecord extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#FFF",
-        marginTop: StatusBarH,
+        // backgroundColor: "red",
     },
     text: {
         color: "#161924",
         fontSize: 20,
         fontWeight: "500"
     }
+});
+
+const overStyle = StyleSheet.create({
+    topLeftSection: {
+        position: "absolute",
+        top: StatusBarH + 10,
+        left: 10,
+        // width: 100,
+        // height: 100,
+        // backgroundColor: "rgba(220,220,220,0.5)",
+    },
+    topLeftButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: "rgba(220,220,220,0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    topLeftImg: {
+        width: 30,
+        height: 30,
+    },
 });
--- screens/main/Wallet.js
+++ screens/main/Wallet.js
@@ -1,22 +1,32 @@
 import React from "react";
-import { AsyncStorage, View, Text, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { Image, View, Text, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
+import img_drawer from '../../assets/images/drawer.png';
 
 const StatusBarH = StatusBar.currentHeight;
 
-export default class wallet extends React.Component {
+export default class Wallet extends React.Component {
     render() {
-
+        const { navigation } = this.props;
         _logout = () => {
             AsyncStorage.clear();
             this.props.navigation.navigate("AuthChecker");
         }
         return (
             <View style={styles.container}>
+                {/** 왼쪽 상단 버튼 */}
+                <View style={overStyle.topLeftSection}>
+                    <TouchableOpacity
+                        style={overStyle.topLeftButton}
+                        onPress={navigation.openDrawer}>
+                        <Image style={overStyle.topLeftImg} source={img_drawer} />
+                    </TouchableOpacity>
+                </View>
+
+
                 <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                     <Text style={styles.text}>{this.props.name} 지갑</Text>
-                    <TouchableOpacity onPress={_logout}><Text>logout</Text></TouchableOpacity>
                 </View>
+                <TouchableOpacity onPress={_logout}><Text>logout</Text></TouchableOpacity>
 
             </View>
         );
@@ -26,12 +36,33 @@ export default class wallet extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#FFF",
-        marginTop: StatusBarH,
+        // backgroundColor: "red",
     },
     text: {
         color: "#161924",
         fontSize: 20,
         fontWeight: "500"
     }
+});
+
+const overStyle = StyleSheet.create({
+    topLeftSection: {
+        position: "absolute",
+        top: StatusBarH + 10,
+        left: 10,
+        // width: 100,
+        // height: 100,
+        // backgroundColor: "rgba(220,220,220,0.5)",
+    },
+    topLeftButton: {
+        width: 40,
+        height: 40,
+        backgroundColor: "rgba(220,220,220,0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    topLeftImg: {
+        width: 30,
+        height: 30,
+    },
 });
